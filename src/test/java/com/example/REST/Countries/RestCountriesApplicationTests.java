e851d41766be0b567e3f16073a9a2b99fda7d04c
PerZZi <emiliano.estebandiaz@gmail.com>
2024-11-13 10:53:05 -0300

    primer commit

---
+++ src/test/java/com/example/REST/Countries/RestCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.REST.Countries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
