e933d66f89990030a8ee9aeef399b51b16baa33e
PerZZi <emiliano.estebandiaz@gmail.com>
2024-11-18 12:03:17 -0300

    se agrego controller de auth para login con jwt

---
+++ src/main/java/com/example/REST/Countries/dtos/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.example.REST.Countries.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String jwt;
+}
---
+++ src/main/java/com/example/REST/Countries/dtos/UsuarioLogin.java
@@ -0,0 +1,12 @@
+package com.example.REST.Countries.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioLogin {
+
+    private String email;
+    private String password;
+}
